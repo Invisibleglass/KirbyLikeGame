fde58538ce269ae75cb5839fd98ae2775ff99768
Invisibleglass <122887946+Invisibleglass@users.noreply.github.com>
2023-02-22 09:59:48 -0700

    Beam now attached
    Now we need to still deactivated it

--- Assets/Scripts/Misc/Swingbeam.cs
+++ Assets/Scripts/Misc/Swingbeam.cs
@@ -6,28 +6,36 @@ public class Swingbeam : MonoBehaviour
 {
     SpriteRenderer sr;
 
+    public GameObject beam;
     public Transform spawnPointRight;
     public Transform spawnPointLeft;
+    public float lifetime;
 
-    public Beam beamPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
-        if (!spawnPointLeft || !spawnPointRight || !beamPrefab)
+        sr = GetComponent<SpriteRenderer>();
+
+        if (!spawnPointLeft || !spawnPointRight || !beam)
             Debug.Log("Please setup default values on " + gameObject.name);
+
     }
 
-    /*public void Fire()
+    public void Swing()
     {
         if (!sr.flipX)
         {
-            Beam curProjectile = Instantiate(beamPrefab, spawnPointRight.position, spawnPointRight.rotation);
+            beam.transform.position = spawnPointRight.position;
+            beam.transform.rotation = spawnPointRight.rotation;
+            beam.SetActive(true);
+            beam.GetComponent<Animator>().Play("SwipeRight");
         }
         else
         {
-            Beam curProjectile = Instantiate(beamPrefab, spawnPointLeft.position, spawnPointLeft.rotation);
-            curProjectile.GetComponent<SpriteRenderer>().flipX = true;
+            beam.transform.position = spawnPointLeft.position;
+            beam.transform.rotation = spawnPointLeft.rotation;
+            beam.SetActive(true);
+            beam.GetComponent<Animator>().Play("SwipeLeft");
         }
-    }*/
+    }
 }
